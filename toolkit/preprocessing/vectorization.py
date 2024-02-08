ca870464e28e3a2f52bf1709c7863c3a1b39518b
Yuri Nascimento <yurinascimennto@gmail.com>
2024-02-08 19:11:21 -0300

    tentativa de documentação do método apply_vectorization

--- toolkit/preprocessing/vectorization.py
+++ toolkit/preprocessing/vectorization.py
@@ -6,12 +6,24 @@ from sklearn.feature_extraction.text import CountVectorizer
 def apply_vectorization(data, content_header, vocabulary=None,
                         percentiles=(5, 80), ngrams=(1, 2),
                         save_vocab_as=None):
+    """
+    Aplica o processo de vetorização, convertendo uma coleção de texto para uma matriz de contagem de tokens, tal que:
+        * Cada linha da matriz é um documento da coleção.
+        * Cada coluna da matriz é um token do vocabulário.
+        * O vocabulário nada mais é do que um array de tokens que aparecem nos textos, tal que esses tokens não se repetem no vocabulário.
+        * O vocabulário depende do parâmetro 'ngrams', que especifica quantos termos terá cada token.
+    -----
+    'term_frequency' é a matriz de contagem de tokens.
+        O elemento MxN diz que o documento M possui (n) vezes o token N, tal que (n) é um inteiro. Ou seja, um matriz (Documento x Token).
+    'features' é o vocabulário de tokens.
+    """
+
     content = data[content_header].copy()
     min_frequency = int(data.shape[0] * percentiles[0] / 100)
     max_frequency = int(data.shape[0] * percentiles[1] / 100)
 
     vectorizer = CountVectorizer(max_features=None, vocabulary=vocabulary,
-                                 min_df=min_frequency, max_df=max_frequency,
+                                 min_df=max(min_frequency, 1), max_df=max_frequency,
                                  ngram_range=ngrams, lowercase=False,
                                  tokenizer=(lambda x: x))
 
